79d052b2a7d82afe6d3617139aae2077a1e96ea3
l_yi <l_yi@gionee.com>
2018-01-03 10:48:49 +0800

    删除PointView
    Change-Id: If45e5cf6864a0c59048c4739ce647365a2c1ac25

--- app/src/main/java/com/example/pinchchicken/PinchActivity.java
+++ app/src/main/java/com/example/pinchchicken/PinchActivity.java
@@ -1,11 +1,7 @@
 package com.example.pinchchicken;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.media.AudioAttributes;
-import android.media.AudioManager;
 import android.media.SoundPool;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
